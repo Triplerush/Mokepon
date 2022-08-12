40d8f9807b3d89d8db0b2b16766d486ff8dec03c
Triplerush <fcanal@unsa.edu.pe>
2022-08-11 21:37:29 -0500

    Agregando logica de escoger la mascota del jugador y aleatoriamente la mascota del enemigo

--- js/script.js
+++ js/script.js
@@ -1,10 +1,21 @@
 function seleccionarMascota() {
   let botonMascota = document.getElementById("boton-mascota");
-  botonMascota.addEventListener("click", saludar);
+  botonMascota.addEventListener("click", mascotaSeleccionada);
 }
 
-function saludar(){
-    alert("hola")
+function mascotaSeleccionada(){
+  let mascotas = ["Hypodoge","Catipepo","Ratigueya","Langostelvis","Tucapalma","Pydos"];
+  let opcion = document.querySelector('input[name=mascota]:checked');
+  let spanMascota = document.getElementById('mascota-jugador');
+  let spanMascotaEnemigo = document.getElementById('mascota-enemigo');
+
+  if(opcion != null){
+    spanMascota.innerText = mascotas[opcion.value];
+    let numAleatorio = Math.ceil(Math.random()*(mascotas.length-1));
+    spanMascotaEnemigo.innerText = mascotas[numAleatorio]
+  }else
+    console.log('Selecciona una mascota');
 }
+
 
 window.addEventListener("load", seleccionarMascota);
